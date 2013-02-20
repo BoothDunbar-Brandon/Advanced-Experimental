470fdea15255e2c1d905335e376e671464fea710
Ekta Patel <ektapatel@Ektas-MacBook-Air.local>
2013-02-20 18:42:32 -0500

    changed code for plotting

--- relativistic_electron.py
+++ relativistic_electron.py
@@ -1,7 +1,10 @@
 import csv
+import matplotlib.pyplot as plt
 
-c=open("relativistic_electron.csv", "rb")
+#c=open("relativistic_electron.csv", "rb")
+c=open("data.txt", "r")
 d=c.readlines()
+c.close()
 x=[]
 y=[]
 for line in d:
@@ -9,4 +12,15 @@ for line in d:
     print a
     x.append(float(a[0]))
     y.append(float(a[1]))
+    break
 print x,y
+plt.plot(x,y)
+plt.xlabel('B field (kG)')
+plt.ylabel('Counts/5 min')
+plt.show()
+
+#add error bars for B field
+
+"""first column is B field in kG, second is # of counts/5 min 
+|error margin in kG is +/-.0002
+|Amplifier - fine = 9.5, coarse = 20   then discriminator Delta E = 0.5 V"""
