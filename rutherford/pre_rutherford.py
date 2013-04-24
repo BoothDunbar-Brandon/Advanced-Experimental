b3688811332511df782294cce464099bfc7f3ae5
Ekta Patel <ektapatel@172-26-29-139.dynapool.nyu.edu>
2013-04-24 15:58:45 -0400

    new stuffs

--- rutherford/pre_rutherford.py
+++ rutherford/pre_rutherford.py
@@ -4,27 +4,16 @@ import matplotlib.pyplot as plt
 #given in manual
 r1=(2.30+2.70)/2
 
-#retrieve values for theta1 which changes as we change Y
-def theta1(Y):
-    return (1/np.arctan(Y/r1))
 
-Y=np.arange(0,20,0.01)
-Y=theta1(Y)
+def f(Y):
+    a=Y/np.sqrt(Y**2+r1**2)
+    b=r1**2/(Y**2+r1**2)
+    X=7.22
+    return a*b/np.sin(0.5*(np.arctan(r1/X)+np.arctan(r1/Y)))**4
 
-#fixed number from geometry of setup
-theta2=np.arcsin(r1/7.22)
-print theta2
-
-
-def func(theta1):
-    return np.cos(theta2)*(np.sin(theta2))**2/(np.sin((theta1+theta2)/2))**4
-
-print func(Y)
-print theta1(Y)
-#this should really be plotting Y vs, f(Y) since theta is a function of Y
-plt.plot(Y,func(Y))
-#plt.xlim(0,20)
-#plt.ylim(0,100000)
+Y=np.arange(1,20,0.01)
+plt.plot(Y,f(Y))
 plt.xlabel('Y (cm)')
 plt.ylabel('f(Y)')
-plt.show()
+plt.savefig('preliminary_plot.pdf')
+#plt.show()
